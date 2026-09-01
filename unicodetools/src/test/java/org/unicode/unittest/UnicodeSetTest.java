aa651f6507c259b2a12ce37ac2a95a327b568349
Robin Leroy <egg.robin.leroy@gmail.com>
2026-09-01 14:13:01 +0200

    Some tests

---
+++ unicodetools/src/test/java/org/unicode/unittest/UnicodeSetTest.java
@@ -0,0 +1,84 @@
+package org.unicode.unittest;
+
+import java.text.ParsePosition;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+import org.unicode.cldr.draft.FileUtilities;
+import org.unicode.props.IndexUnicodeProperties;
+import org.unicode.props.UnicodeProperty;
+import org.unicode.text.UCD.VersionedSymbolTable;
+import org.unicode.text.utility.Settings;
+import org.unicode.text.utility.Utility;
+
+import com.ibm.icu.text.UnicodeSet;
+
+public class UnicodeSetTest extends TestFmwkMinusMinus {
+    @Test
+    void testLatest() {
+        final var iup = IndexUnicodeProperties.make();
+        final var symbolTable = VersionedSymbolTable.forDevelopment();
+        String path =
+                org.unicode.text.utility.Utility.getMostRecentUnicodeDataFile(
+                        "unicodeset/*/UnicodeSetTest", Settings.latestVersion, true, false);
+        for (final String line : FileUtilities.in("", path)) {
+          System.out.println(line);
+            final int commentPosition = line.indexOf('#');
+            final String contents = commentPosition >= 0 ? line.substring(0, commentPosition) : line;
+            if (contents.isEmpty()) {
+                continue;
+            }
+            final String[] fields = Arrays.stream(contents.split(";")).map(String::strip).toArray(String[]::new);
+            final var scope = fields[0];
+            final var general = fields[1];
+            final var properties =
+                    Arrays.stream(fields[2].split(" "))
+                            .map(iup::getProperty)
+                            .toArray(UnicodeProperty[]::new);
+            final var elements = fields[3].isEmpty() ? new String[] {} :
+                    Arrays.stream(fields[3].replaceFirst("^ *<", "").replaceFirst("> *$", "").split("> <"))
+                            .map(Utility::fromHex)
+                            .toArray(String[]::new);
+            final var nonElements = fields[4].isEmpty() ? new String[] {} :
+                    Arrays.stream(fields[4].replaceFirst("^ *<", "").replaceFirst("> *$", "").split("> <"))
+                            .map(Utility::fromHex)
+                            .toArray(String[]::new);
+            final Integer size = fields[5].isBlank() ? null : Integer.parseInt(fields[5]);
+            final var expression = fields[6];
+            final var pp = new ParsePosition(0);
+            if (scope.equals("Ill_Formed")) {
+              assertEquals("Ill-formed test must not expect elements",0, elements.length);
+              assertEquals("Ill-formed test must not expect non-elements", 0, nonElements.length);
+              assertEquals("Ill-formed test must not expect size", null,size);
+            }
+            UnicodeSet setUnderTest = null;
+            try {
+              setUnderTest = new UnicodeSet(expression, pp, symbolTable);
+              if (scope.equals("Ill_Formed")) {
+                System.out.println("EXTENSION: " + expression + " = " + setUnderTest.complement().complement());
+              }
+            } catch (Exception e) {
+              if (e.getMessage().contains("doubly negated property-query")) {
+                  System.out.println("RESTRICTION: " + e.getMessage() + " for " + line);
+              } else if (!scope.equals("Ill_Formed")) {
+                errln("Parse error " + e.getMessage() + " for " + line);
+              }
+                  continue;
+            }
+              for (final String element : elements) {
+                if (!setUnderTest.contains(element)) {
+                 errln("element <" + Utility.hex(element) + "> " + element + " for " + line);
+                }
+              }
+              for (final String element : nonElements) {
+                if (setUnderTest.contains(element)) {
+                errln("non-element <" + Utility.hex(element) + "> " + element + " for " + line);
+                }
+              }
+              if (size != null) {
+                if (setUnderTest.size() != size) {
+                  errln("size is " +setUnderTest.size() +" for " + line);
+                }
+              }
+        }
+    }
+}
